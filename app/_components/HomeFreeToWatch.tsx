e1cbc93b28c502eaa1d08960f0da1533637823a4
bama <yosafatbama.arintoko@gmail.com>
2025-02-03 14:44:06 +0700

    done

--- app/_components/HomeFreeToWatch.tsx
+++ app/_components/HomeFreeToWatch.tsx
@@ -1,16 +1,88 @@
-import { oswald } from "@/lib/fonts";
+'use client'
+import { oswald, poppins } from "@/lib/fonts";
 import Selector from "./Selector";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react"
+import Image from "next/image";
+interface Movie {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[]; // You can further define this if you have specific genres
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    name: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+interface Dates {
+    maximum: string;
+    minimum: string;
+}
+
+interface ApiResponse {
+    dates: Dates;
+    page: number;
+    results: Movie[];
+}
+
+interface Selectors {
+    label: string;
+    value: string;
+}
+
+const MOVIES = 'movie'
+const TV = 'tv'
 const selectorArr = [
     {
         label: "Movies",
-        value: ""
+        value: MOVIES
     },
     {
         label: "TV",
-        value: ""
+        value: TV
     }
 ]
+
+
 export default function HomeFreeToWatch() {
+    const [freeMovie, setFreeMovie] = useState<Movie[]>([])
+    useEffect(() => {
+        fetchApi(MOVIES)
+    }, [])
+
+    const handleChange = () => (value: Selectors) => {
+        switch (value.value) {
+            case MOVIES:
+                fetchApi(MOVIES)
+                break;
+
+            default:
+                fetchApi(TV)
+                break;
+        }
+    }
+
+    const fetchApi = async (params: string) => {
+        try {
+            // console.log('params : ', params)
+            const fetchResponse = await fetch(`${process.env.NEXT_PUBLIC_TMDB_HOST}discover/${params}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&watch_region=US&with_watch_monetization_types=free`)
+            const dataResponse: ApiResponse = await fetchResponse.json()
+            // console.log('dataResponse : ', dataResponse)
+            if (fetchResponse.ok) {
+                setFreeMovie(dataResponse.results)
+            }
+        } catch (error) {
+            console.log('error : ', error)
+        }
+    }
     return (
         <div className="w-full">
             <div className="2xl:max-w-[1280px] mx-auto h-auto lg:flex justify-center-center flex-col" >
@@ -20,7 +92,35 @@ export default function HomeFreeToWatch() {
                         Free To Watch
 
                     </p>
-                    <Selector data={selectorArr}/>
+                    <Selector data={selectorArr} onChange={handleChange()} />
+                </div>
+                <div className="relative h-[calc(14rem+4rem)]">
+                    {
+                        freeMovie.length > 0
+                        &&
+                        <div className="w-full flex overflow-x-auto flex-nowrap lg:space-x-5 xl:space-x-5 2xl:space-x-5 space-x-4 lg:px-10 xl:px-10 2xl:px-10 px-4 h-[calc(14rem+4rem)] overflow-y-hidden">
+                            {freeMovie.map((x, y) => (
+                                <div key={y} className="w-36 flex-shrink-0 mt-3 space-y-1">
+                                    <motion.div whileHover={{ scale: 1.05 }} key={y} className="relative h-56">
+                                        <Image
+                                            priority
+                                            src={`https://image.tmdb.org/t/p/w220_and_h330_smart${x.poster_path}`} // Replace with your dynamic poster path
+                                            alt="Movie Poster"
+                                            fill
+                                            style={{ objectFit: "cover" }}
+                                            className="object-cover rounded-md"
+                                        />
+                                    </motion.div>
+                                    <div className=" h-14">
+                                        <p className={`${poppins.className} font-bold text-sm line-clamp-2`}>
+
+                                            {x.title || x.name}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    }
                 </div>
                 {/* <div>
         
--- app/_components/HomePopular.tsx
+++ app/_components/HomePopular.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { oswald, poppins } from "@/lib/fonts";
 import Selector from "./Selector";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface Movie {
     adult: boolean;
@@ -14,6 +16,7 @@ interface Movie {
     poster_path: string;
     release_date: string;
     title: string;
+    name: string;
     video: boolean;
     vote_average: number;
     vote_count: number;
@@ -29,27 +32,88 @@ interface ApiResponse {
     page: number;
     results: Movie[];
 }
+
+interface Selectors {
+    label: string;
+    value: string;
+}
+
+const STREAMING = 'streaming'
+const ONTV = 'on_tv'
+const FORRENT = "for_rent"
+const INTHEATERS = 'in_theaters'
 const selectorArr = [
     {
         label: "Streaming",
-        value:""
+        value: STREAMING
     },
     {
         label: "On TV",
-        value:""
+        value: ONTV
     },
     {
         label: "For Rent",
-        value:""
+        value: FORRENT
     },
     {
         label: "In Theaters",
-        value:""
+        value: INTHEATERS
     }
 ]
-export default async function HomePopular() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_TMDB_HOST}movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`)
-    const data: ApiResponse = await res.json();
+export default function HomePopular() {
+    const [populars, setPopulars] = useState<Movie[]>([])
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_TMDB_HOST}movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`)
+    // const data: ApiResponse = await res.json();
+
+    useEffect(() => {
+        fetchApi('movie', 8)
+    }, [])
+
+    const handleChange = () => (value: Selectors) => {
+        switch (value.value) {
+            case STREAMING:
+                fetchApi('movie', 8)
+                break;
+            case ONTV:
+                fetchApi('tv', 15)
+                break;
+            case FORRENT:
+                fetchApi('movie', 10)
+                break;
+            default:
+                fetchInTheaters()
+                break;
+        }
+    }
+
+    const fetchApi = async (params: string, providers: number) => {
+        console.log('params : ', params)
+        console.log('providers : ', providers)
+        try {
+            const moviesResponse = await fetch(`${process.env.NEXT_PUBLIC_TMDB_HOST}discover/${params}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&with_watch_providers=${providers}&watch_region=US`);
+            const movieData: ApiResponse = await moviesResponse.json()
+            if (moviesResponse.ok) {
+                // console.log('----- ', movieData.results)
+                setPopulars(movieData.results)
+            }
+            // console.log("movieData : ", movieData)
+        } catch (error) {
+            console.log('error : ', error)
+        }
+    }
+
+    const fetchInTheaters = async () => {
+        try {
+            const theatersResponse = await fetch(`${process.env.NEXT_PUBLIC_TMDB_HOST}movie/now_playing?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`)
+            const theatersData: ApiResponse = await theatersResponse.json()
+            if (theatersResponse.ok) {
+                setPopulars(theatersData.results)
+            }
+            // console.log('theatersData : ', theatersData)
+        } catch (error) {
+            console.log('error : ', error)
+        }
+    }
     return (
         <div className="w-full">
             <div className="2xl:max-w-[1280px] mx-auto h-auto lg:flex justify-center-center flex-col" >
@@ -58,12 +122,12 @@ export default async function HomePopular() {
 
                         What&apos;s Popular
                     </p>
-                    <Selector data={selectorArr}/>
+                    <Selector data={selectorArr} onChange={handleChange()} />
                 </div>
                 <div>
 
                     <div className="w-full flex overflow-x-auto flex-nowrap space-x-5 lg:px-10 xl:px-10 2xl:px-10 px-4">
-                        {data.results.map((x, y) => (
+                        {populars.map((x, y) => (
                             <div key={y} className="w-36 flex-shrink-0">
                                 <div key={y} className="relative">
                                     <Image
@@ -76,9 +140,9 @@ export default async function HomePopular() {
                                     />
                                 </div>
                                 <div>
-                                    <p className={`${poppins.className} font-bold text-[16px]`}>
+                                    <p className={`${poppins.className} font-bold text-sm line-clamp-2`}>
 
-                                        {x.title}
+                                        {x.title || x.name}
                                     </p>
                                 </div>
                             </div>
